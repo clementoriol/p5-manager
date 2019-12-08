19fa317919dbbf98d66aa3b7a3c046536f5c3f40
Clément Oriol <oriol.clement@gmail.com>
2019-12-08 17:18:18 +0100

    Update sidebar to fix weird margin issues

--- src/Sidebar.vue
+++ src/Sidebar.vue
@@ -18,256 +18,254 @@
 </template>
 
 <script>
-  import Vue from 'vue'
-  import VueRouter from 'vue-router'
-  import $ from 'jquery'
-  Vue.use(VueRouter);
+import Vue from "vue";
+import VueRouter from "vue-router";
+import $ from "jquery";
+Vue.use(VueRouter);
 
-  export default {
-    data() {
-      return {
-        projects: [],
-        collectionName: '',
-        sidebarActive: false
+export default {
+  data() {
+    return {
+      projects: [],
+      collectionName: "",
+      sidebarActive: false
+    };
+  },
+  props: ["side", "split"],
+  computed: {
+    activeProject() {
+      return this.$route.params.project;
+    }
+  },
+  methods: {
+    toggle() {
+      if (this.sidebarActive) {
+        this.sidebarActive = false;
+      } else {
+        this.sidebarActive = true;
       }
+      console.log(this.sidebarActive);
     },
-    props: ['side', 'split'],
-    computed: {
-      activeProject() {
-        return this.$route.params.project
+    staticLinkTo(project) {
+      return document.location.origin + "/" + project + "/index.html";
+    },
+    fullLinkTo(side, project) {
+      var projects = this.$route.params.twoProjects.toString().split("+");
+
+      if (side === "left") {
+        return "/split-view/" + project + "+" + projects[1];
+      } else {
+        return "/split-view/" + projects[0] + "+" + project;
       }
-    },
-    methods: {
-      toggle() {
-
-        if (this.sidebarActive) {
-          this.sidebarActive = false;
-        }
-        else {
-          this.sidebarActive = true;
-        }
-        console.log(this.sidebarActive)
-      },
-      staticLinkTo(project) {
-        return document.location.origin + '/' + project + '/index.html'
-      },
-      fullLinkTo(side, project) {
-        var projects = this.$route.params.twoProjects.toString().split('+')
-
-        if (side === 'left') {
-          return '/split-view/' + project + '+' + projects[1]
-        }
-
-        else {
-          return '/split-view/' + projects[0] + '+' + project
-        }
+    }
+  },
+  mounted: function() {
+    console.log("going to fetch");
+    var self = this;
+    $.get({
+      url: "/api/p5rc",
+      dataType: "json",
+      success: function(data) {
+        self.projects = data.projects;
+        self.collectionName = data.collectionName;
       }
-    },
-    mounted: function() {
-      console.log('going to fetch');
-      var self = this;
-      $.get({
-        url: '/api/p5rc',
-        dataType: 'json',
-        success: function(data) {
-          self.projects = data.projects;
-          self.collectionName = data.collectionName;
-        }
-      })
-    },
+    });
   }
+};
 </script>
 
 <style lang="scss">
-  a {
-    color: #999;
-    text-decoration: none;
+a {
+  color: #999;
+  text-decoration: none;
 
-    &:hover, &:active {
-      color: #555;
-    }
-
-    &:active {
-      color: #f07;
-    }
+  &:hover,
+  &:active {
+    color: #555;
   }
 
-  .sidebar {
-    position: absolute;
-    z-index: 10;
-    top: 0;
-    background-color: #f5f5f5;
-    width: 220px;
-    height: 100%;
-    text-align: left;
+  &:active {
+    color: #f07;
+  }
+}
+
+.sidebar {
+  position: absolute;
+  z-index: 10;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0);
+  width: 308px;
+  height: 100%;
+  text-align: left;
+  transition: all 0.5s;
+  -webkit-transition: all 0.5s;
+
+  h2 {
+    margin-top: 10px;
+    padding-left: 20px;
+    color: #333;
+  }
+
+  ul {
+    list-style: none;
+    padding-left: 20px;
+  }
+
+  &.sidebar--active {
     transition: all 0.5s;
-    -webkit-transition: all 0.5s;
+  }
+
+  .project-link--active {
+    color: #f07;
+  }
+
+  .staticBtn {
+    color: #ccc;
+    font-size: 15px;
+
+    .active {
+      color: #aaa;
+    }
+  }
+}
+
+.toggle {
+  position: absolute;
+  top: 10px;
+  background-color: transparent;
+  border: none;
+  transition: all 1s;
+  -webkit-transition: all 1s;
+  z-index: 11;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  img {
+    width: 24px;
+  }
+
+  &.toggle--left {
+    left: 180px;
+  }
+
+  &.toggle--right {
+    right: 180px;
+  }
+
+  &.toggle--active {
+    -ms-transform: rotate(144deg); /* IE 9 */
+    -webkit-transform: rotate(144deg); /* Chrome, Safari, Opera */
+    transform: rotate(144deg);
+  }
+}
+
+.about {
+  position: absolute;
+  bottom: 5px;
+  left: 0;
+  right: 0;
+  font-size: 14px;
+  padding-left: 20px;
+
+  p {
+    margin-top: 0;
+    color: #999;
+  }
+
+  .twitter {
+    color: #f07;
+  }
+}
+
+.link-split {
+  display: block;
+  color: #f07;
+}
+
+.split-view--left .sidebar {
+  left: -255px;
+  text-align: left;
+}
+
+.split-view--right .sidebar {
+  right: -255px;
+  text-align: right;
+}
+
+.split-view--left .sidebar.sidebar--active {
+  background-color: #f5f5f5;
+  left: 0;
+}
+
+.split-view--right .sidebar.sidebar--active {
+  background-color: #f5f5f5;
+  right: 0;
+}
+
+.split-view--left .sidebar h2 {
+  margin-top: 10px;
+  padding-left: 20px;
+}
+
+.split-view--right .sidebar h2 {
+  margin-top: 10px;
+  padding-right: 20px;
+}
+
+.sidebar h2 a {
+  color: #333;
+}
+
+.split-view--left .sidebar ul {
+  list-style: none;
+  padding-left: 20px;
+}
+
+.split-view--right .sidebar ul {
+  list-style: none;
+  padding-right: 20px;
+}
+
+.split-view.split-view--left .toggle {
+  left: 260px;
+}
+
+.split-view.split-view--right .toggle {
+  right: 260px;
+}
+
+.split-view.split-view--right .about {
+  display: none;
+}
+
+.single-view {
+  .sidebar {
+    left: -255px;
+    text-align: left;
+
+    &.sidebar--active {
+      background-color: #f5f5f5;
+      left: 0;
+    }
 
     h2 {
       margin-top: 10px;
-      padding-left: 30px;
-      color: #333;
+      padding-left: 20px;
     }
 
     ul {
       list-style: none;
-      padding-left: 30px;
+      padding-left: 20px;
     }
-
-    // &.sidebar--active {
-    //   left: 0;
-    // }
-
-    .project-link--active {
-      color: #f07;
-    }
-
-    .staticBtn {
-      color: #ccc;
-      font-size: 15px;
-
-      .active {
-        color: #aaa;
-      }
+    .toggle {
+      left: 260px;
     }
   }
-
-  .toggle {
-    position: absolute;
-    top: 10px;
-    background-color: transparent;
-    border: none;
-    transition: all 1s;
-    -webkit-transition: all 1s;
-    z-index: 11;
-
-    &:hover {
-      cursor: pointer;
-    }
-
-    &:focus {
-      outline: none;
-    }
-
-    img {
-      width: 24px;
-    }
-
-    &.toggle--left {
-      left: 180px;
-
-    }
-
-    &.toggle--right {
-      right: 180px;
-
-    }
-
-    &.toggle--active {
-      -ms-transform: rotate(144deg); /* IE 9 */
-      -webkit-transform: rotate(144deg); /* Chrome, Safari, Opera */
-      transform: rotate(144deg);
-    }
-  }
-
-  .about {
-    position: absolute;
-    bottom: 5px;
-    left: 0;
-    right: 0;
-    font-size: 14px;
-    padding-left: 30px;
-
-    p {
-      margin-top: 0;
-      color: #999;
-    }
-
-    .twitter {
-      color: #f07;
-    }
-  }
-
-  .link-split {
-    display: block;
-    color: #f07;
-  }
-
-  .split-view--left .sidebar {
-    left: -175px;
-    text-align: left;
-  }
-
-  .split-view--right .sidebar {
-    right: -175px;
-    text-align: right;
-  }
-
-  .split-view--left .sidebar.sidebar--active {
-    left: 0;
-  }
-
-  .split-view--right .sidebar.sidebar--active {
-    right: 0;
-  }
-
-  .split-view--left .sidebar h2 {
-    margin-top: 10px;
-    padding-left: 30px;
-  }
-
-  .split-view--right .sidebar h2 {
-    margin-top: 10px;
-    padding-right: 30px;
-  }
-
-  .sidebar h2 a {
-    color: #333;
-  }
-
-  .split-view--left .sidebar ul {
-    list-style: none;
-    padding-left: 30px;
-  }
-
-  .split-view--right .sidebar ul {
-    list-style: none;
-    padding-right: 30px;
-  }
-
-  .split-view.split-view--left .toggle{
-    left: 180px;
-  }
-
-  .split-view.split-view--right .toggle{
-    right: 180px;
-  }
-
-  .split-view.split-view--right .about {
-    display: none;
-  }
-
-  .single-view {
-    .sidebar {
-      left: -175px;
-      text-align: left;
-
-      &.sidebar--active {
-        left: 0;
-      }
-
-      h2 {
-        margin-top: 10px;
-        padding-left: 30px;
-      }
-
-      ul {
-        list-style: none;
-        padding-left: 30px;
-      }
-      .toggle{
-        left: 180px;
-      }
-    }
-  }
+}
 </style>
